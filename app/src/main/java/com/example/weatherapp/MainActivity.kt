f247cdcafd93a76254a008c88f8c2b4dec312487
Xacca <Xacca13@gmail.com>
2025-05-14 10:25:17 +0600

    update

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.fragment.MainFragment
-import org.json.JSONObject
-
 
 
 class MainActivity : AppCompatActivity() {
